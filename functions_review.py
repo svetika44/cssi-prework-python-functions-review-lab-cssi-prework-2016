c372278a4f434f05526de92028eae2357f1133dd
svetika44 <sladkova@google.com>
2016-05-28 14:32:16 -0700

    Done.

--- functions_review.py
+++ functions_review.py
@@ -1,7 +1,31 @@
 
 #Division Function: given two numbers returns quotient of the first number divided by the second number
 def division(num1, num2):
-    return "your code here"
+    return num1/num2
+
+    print division(42, 7)
+
+
+def greeting(self):
+  print 'Hi There','+self+','!'
+  greeting ("Susan");
+
+  print 'Greetings', '+self+', '!'
+  greeting("Earthling")
+
+
+def return_a_value(self):
+  return self
+
+  print return_a_value("Have a nice day!")
+
+
+     def pizza_party(self = "Cheese"):
+      return "pizza party",'+self+' 
+     
+    print pizza_party ("Pepperoni")
+
+   
 
 #Uncomment the rest of these functions and get them to pass the tests!  
 
@@ -9,8 +33,7 @@ def division(num1, num2):
 # def greeting:
 
 # Return a value: Returns the string "Have a nice day!"
-# def return_a_value:
-    
+# def return_a_value: 
 
 #Pizza Party: Has an optional argument that defaults to "cheese" and returns "cheese" when called without an argument
 
